45f01467d30cbd79bcff081fe6593b62082f4090
Katie Assoian <ktassoian@gmail.com>
2022-11-03 20:18:17 -0400

    single product action creator

--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -1,3 +1,10 @@
+import axios from 'axios';
 
+const SET_SINGLE_PRODUCT = 'SET_SINGLE_PRODUCT';
 
-
+export const setSingleProduct = (product) => {
+  return {
+    type: SET_SINGLE_PRODUCT,
+    product,
+  };
+};
